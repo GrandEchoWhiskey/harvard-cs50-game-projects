d209b6516259e8e0c1e3c680c0583b5b4c2fc8ba
GrandEchoWhiskey <pv.wolf.eric@gmail.com>
2023-02-09 20:49:57 +0100

    Edited muzzle elevation script

---
+++ Tanks/Assets/VehicleControll.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class VehicleControll : MonoBehaviour
+{
+    public Rigidbody mRigidbody;
+    public float moveSpeed = 5f;
+    public float rotateSpeed = 3f;
+
+    void Start()
+    {
+        if(mRigidbody == null) mRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        move();
+        rotate();
+    }
+
+    void move()
+    {
+        Vector3 mInput = transform.rotation * (Vector3.forward * Input.GetAxis("Vertical"));
+        mRigidbody.MovePosition(transform.position + mInput * Time.fixedDeltaTime * moveSpeed);
+    }
+
+    void rotate()
+    {
+        Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, Input.GetAxis("Horizontal") * 50 * rotateSpeed / 3f, 0) * Time.fixedDeltaTime);
+        mRigidbody.MoveRotation(mRigidbody.rotation * deltaRotation);
+    }
+}
